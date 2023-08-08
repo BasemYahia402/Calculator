4045bc8b1aee5d00487ba8edd875907aa2c609cc
BasemYahia402 <basemyahia70@gmail.com>
2023-08-08 12:20:03 +0300

    add declaration variable

---
+++ assets/js/mainScript.js
@@ -0,0 +1,7 @@
+let hist = JSON.parse(localStorage.getItem('history')) || [];
+const sidebarToggle = document.getElementById('sidebar-toggle');
+const sidebarCancel= document.getElementById('sidebar-cancel');
+const sidebar = document.getElementById('sidebar');
+const calculator = document.getElementById('calculator');
+const currencyButton = document.getElementById('currency-button');
+const standardButton = document.getElementById('standard-button');
